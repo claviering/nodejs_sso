ae6d18dda6b23f5d188f4baeccc3447ed8f28d45
claviering <claviering@gmail.com>
2019-01-02 09:00:16 +0800

    save

---
+++ README.md
@@ -0,0 +1,9 @@
+# SSO
+
+Nodejs 实现 SSO
+
+参考
+
+> [bradtraversy](https://github.com/bradtraversy/node_passport_login)
+
+> [allen.chen](https://github.com/chsipeng)
---
+++ sso.js
@@ -0,0 +1,29 @@
+const app = require('express')();
+const debug = require('debug')('sso:app');
+const cluster = require('cluster');
+const numCPUs = require('os').cpus().length;
+const router = require('./sso/router');
+const middleware = require('./sso/middleware');
+const config = require('./sso/env');
+
+middleware(app);
+router(app)
+app.listen(config.port, debug(`Server started on http://${config.url}:${config.port}`));
+
+// 多线程
+// if (cluster.isMaster) {
+//   debug(`Master ${process.pid} is running`);
+//   // Fork workers.
+//   for (let i = 0; i < numCPUs; i++) {
+//     cluster.fork();
+//   }
+//   cluster.on('listening', (worker, address) => {
+//     debug(`worker ${worker.process.pid}, listen: http://${config.url}:${config.port}`);
+//   });
+//   cluster.on('exit', (worker, code, signal) => {
+//     debug(`worker ${worker.process.pid} died`);
+//     cluster.fork();
+//   });
+// } else {
+//   app.listen(config.port, debug(`Server started on http://${config.url}:${config.port}`));
+// }
